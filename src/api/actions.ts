9e6b28689bb62d9781d54560b89fbefbf16824e6
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 13:31:35 -0500

    add action

---
+++ src/api/actions.ts
@@ -0,0 +1,13 @@
+/**
+ * action.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/action
+ * @property {bunbuilder.module:bunbuilder/api/action}
+ */
+
+const action = {
+
+}
+
+export default action
