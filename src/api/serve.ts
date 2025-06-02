e9ad92f8aa9615079c26b6b0e60d64ac706ca30e
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 12:11:02 -0500

    add serve

---
+++ src/api/serve.ts
@@ -0,0 +1,13 @@
+/**
+ * serve.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/serve
+ * @property {bunbuilder.module:bunbuilder/api/serve}
+ */
+
+const serve = {
+
+}
+
+export default serve
