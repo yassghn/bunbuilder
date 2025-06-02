657d9bc8b143a875272ab2dcfa9e85b5b1936413
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 12:21:45 -0500

    add clean

---
+++ src/api/clean.ts
@@ -0,0 +1,13 @@
+/**
+ * clean.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/clean
+ * @property {bunbuilder.module:bunbuilder/api/clean}
+ */
+
+const clean = {
+
+}
+
+export default clean
