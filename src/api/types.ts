eb2bb5053723cdd4dbe6741905260ae0a78b719c
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 14:02:03 -0500

    jsdoc strings

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,11 +2,23 @@
  * types.ts
  */
 
+/**
+ * @typedef {object} PARSED_ARGS
+ * @type {PARSED_ARGS} parsed cli arguments
+ * @property {object} values cli arguments
+ * @property {string[]} positionals positional cli aruments
+ */
 interface PARSED_ARGS {
     values: object
     positionals: string[]
 }
 
+/**
+ * @typedef {object} ACTION_PLAN
+ * @type {ACTION_PLAN} bunbuilder action plan
+ * @property {object} actions bunbuilder actions
+ * @property {string[]} files individual files for actions
+ */
 interface ACTION_PLAN {
     actions: object
     files: string[] | undefined
