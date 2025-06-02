ee21f7cb713fbf18bd361eab28a77ffb8e2fae69
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 11:32:35 -0500

    release/bin

--- bin/bunbuilder.js
+++ bin/bunbuilder.js
@@ -1,28 +1,31 @@
 // @bun
-// api/io.ts
-var io = {
-  echo: (str, newline = false) => {
-    const output = { str: str.valueOf() };
-    if (newline) {
-      output.str += `
+// src/api/io.ts
+import { styleText } from "util";
+import { stdout } from "process";
+function _addEchoOptions(str, options) {
+  const output = { str: str.valueOf() };
+  if (options.color) {
+    output.str = styleText(options.color, str, { stream: stdout, validateStream: true });
+  }
+  if (options.newLine) {
+    output.str += `
 `;
-    }
-    Bun.write(Bun.stdout, output.str);
+  }
+  return output.str;
+}
+async function _echo(str, options = undefined) {
+  const output = { str: str.valueOf() };
+  if (options) {
+    output.str = _addEchoOptions(str, options);
+  }
+  await Bun.write(Bun.stdout, output.str);
+}
+var io = {
+  echo: async (str, options = undefined) => {
+    await _echo(str, options);
   }
 };
 var io_default = io;
-
-// api/build.ts
-var build = {
-  greet: () => {
-    const greeting = ["bunbuilder", "greet"];
-    greeting.forEach((str) => io_default.echo(str));
-  },
-  buildAll: (options) => {
-    console.log(options);
-  }
-};
-var build_default = build;
 // package.json
 var package_default = {
   name: "bunbuilder",
@@ -30,39 +33,94 @@ var package_default = {
   description: "build bun apps",
   author: "yassghn@protonmail.com",
   license: "SEE LICENSE IN license",
-  module: "index.ts",
+  module: "build.ts",
   type: "module",
-  scripts: {},
-  bin: "dist/bunbuilder.exe",
+  scripts: {
+    ship: "bun run build && npm pack"
+  },
+  bin: "bin/bunbuilder.js",
   devDependencies: {
     "@types/bun": "latest",
     "@typescript-eslint/parser": "^8.33.0",
-    eslint: "^9.27.0",
+    eslint: "^9.28.0",
     prettier: "^3.5.3",
     "prettier-eslint": "^16.4.2",
-    tar: "^7.4.3",
     typescript: "^5.8.3"
   }
 };
 
-// build/bunbuilder.ts
+// src/api/util.ts
 function _hewPackageInfoString() {
-  const info = [package_default.name, "-", "v", package_default.version];
+  const name = package_default.name;
+  const version = package_default.version;
+  const info = [name, " ", "v", version];
   return info.join("");
 }
-function _bunbuilder(options) {
-  console.log(_hewPackageInfoString());
-  console.log(options);
-}
-function bunbuilder(options) {
-  _bunbuilder(options);
-  build_default.buildAll(options);
-}
-var bunbuilder_default = bunbuilder;
-
-// index.ts
-console.log("bunbuilder");
-var bunbuilder_default2 = bunbuilder_default;
-export {
-  bunbuilder_default2 as default
+var util = {
+  hewPackageInfoString: () => {
+    return _hewPackageInfoString();
+  }
 };
+var util_default = util;
+
+// src/api/build.ts
+function _buildGreet() {
+  const info = util_default.hewPackageInfoString();
+  io_default.echo(info, { newLine: true, color: "green" });
+}
+function _buildAll() {}
+var build = {
+  greet: () => {
+    _buildGreet();
+  },
+  all: () => {
+    _buildAll();
+  }
+};
+var build_default = build;
+// data/data.json
+var data_default = {
+  configFileName: "bunbuilder.config.json"
+};
+
+// src/api/config.ts
+import { readFileSync } from "fs";
+import { cwd } from "process";
+import { sep } from "path";
+var defaults = {
+  name: data_default.configFileName
+};
+function _hewConfigPath() {
+  const absPath = cwd() + sep + defaults.name;
+  return absPath;
+}
+function _importConfig() {
+  const configPath = _hewConfigPath();
+  const data = readFileSync(configPath, "utf-8");
+  const config = JSON.parse(data);
+  return config;
+}
+function _parse() {
+  const config = _importConfig();
+  return config;
+}
+var config = {
+  parse: () => {
+    return _parse();
+  }
+};
+var config_default = config;
+
+// src/bunbuilder.ts
+(async function() {
+  function _bunbuilder() {
+    build_default.greet();
+    const conf = config_default.parse();
+    console.dir(conf);
+  }
+  try {
+    _bunbuilder();
+  } catch (e) {
+    console.error(e);
+  }
+})();
