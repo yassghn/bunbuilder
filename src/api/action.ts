197f5143388f756ee5662d81189bf7c0966c29c4
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 16:20:47 -0500

    bunbuilder action plan processing

--- src/api/action.ts
+++ src/api/action.ts
@@ -6,15 +6,68 @@
  * @property {bunbuilder.module:bunbuilder/api/action}
  */
 
+import { ACTION } from './types'
 import type { ACTION_PLAN, BUNBUILDER_CONFIG } from './types'
+import util from './util'
 
-function _start(actionPlan: ACTION_PLAN, config: BUNBUILDER_CONFIG) {
+/**
+ * invoke help action
+ */
+async function _takeActionHelp() {
+    await util.printHelp()
+}
 
+/**
+ * process action type
+ *
+ * @param {string} action action to invoke
+ */
+async function _processAction(action: string) {
+    switch (action) {
+        case ACTION.build:
+            console.log(action)
+            break
+        case ACTION.clean:
+            console.log(action)
+            break
+        case ACTION.serve:
+            console.log(action)
+            break
+        case ACTION.watch:
+            console.log(action)
+            break
+        case ACTION.help:
+            await _takeActionHelp()
+            break
+    }
+}
+
+/**
+ * process bunbuilder action plan
+ *
+ * @param {ACTION_PLAN} actionPlan bunbuilder actions to process
+ */
+async function _processActions(actionPlan: ACTION_PLAN) {
+    for (const action in actionPlan.actions) {
+        await _processAction(action)
+    }
+}
+
+/**
+ * begin processing bunbuilder action plan with given configuration
+ *
+ * @param {ACTION_PLAN} actionPlan bunbuilder action plan
+ * @param {BUNBUILDER_CONFIG} config bunbuilder configuration
+ */
+async function _start(actionPlan: ACTION_PLAN, config: BUNBUILDER_CONFIG) {
+    await _processActions(actionPlan)
+    //console.dir(actionPlan)
+    //console.dir(config)
 }
 
 const action = {
-    start: (actionPlan: ACTION_PLAN, config: BUNBUILDER_CONFIG) => {
-        _start(actionPlan, config)
+    start: async (actionPlan: ACTION_PLAN, config: BUNBUILDER_CONFIG) => {
+        await _start(actionPlan, config)
     }
 }
 
