fc9ff6990be25290cab50ade210a8902d9bffb1b
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-03 17:31:12 -0500

    infer browser target build operation

--- src/api/buildOp.ts
+++ src/api/buildOp.ts
@@ -6,18 +6,103 @@
  * @property {bunbuilder.module:bunbuilder/api/buildOp} buildOp build operations
  */
 
-import { sep, extname } from 'node:path'
+import buildConfig from './buildConfig'
+import type { BROWSER_BUILD_OP_MAP, BUILD_OP_MAP } from './types'
+import data from '../../data/data.json' assert { type: 'json' }
+import { extname } from 'node:path'
 
-function _inferOperation(dir: string, file: string) {
-    const relativePath = dir + sep + file
-    // ext == '' for extensionless files
-    const ext = extname(relativePath)
-    console.log(ext)
+/**
+ * get pre-configured browser target compile extensions
+ *
+ * @returns {string[]} browser target specific extensions which require compilation
+ */
+function _hewBrowserCompileExts(): string[] {
+    const browser = data.buildTargets.browser
+    const compileExts = browser.compileExts
+    return compileExts
+}
+
+/**
+ * infer build operation for browser specific file extension
+ *
+ * @param {string} ext source file extension
+ * @returns {BROWSER_BUILD_OP_MAP} browser target specific build operation for file extension
+ */
+function _hewBrowserOpMap(ext: string): BROWSER_BUILD_OP_MAP {
+    const compileExts = _hewBrowserCompileExts()
+    const opMap: BROWSER_BUILD_OP_MAP = {
+        ext: ext.valueOf(),
+        op: 'copy'
+    }
+    if (compileExts.includes(ext)) opMap.op = 'compile'
+    return opMap
+}
+
+/**
+ * infer build operations for given extensions based on bunbuilder
+ * configuration build target
+ *
+ * @param {string} ext source file extensions
+ * @returns {BUILD_OP_MAP} inferred build operation for given extension
+ */
+function _inferBuildOpMap(ext: string): BUILD_OP_MAP {
+    const config = buildConfig.state
+    const buildTargets = data.buildTargets
+    const retVal = { opMap: {} as BUILD_OP_MAP }
+    switch (config.target) {
+        case buildTargets.browser.name:
+            const opMap = _hewBrowserOpMap(ext)
+            retVal.opMap = opMap
+            break
+    }
+    return retVal.opMap
+}
+
+/**
+ * list file extensions
+ *
+ * @param {string[]} files source input files
+ * @returns {string[]} list of unique file extensions
+ */
+function _listExtensions(files: string[]): string[] {
+    const extensions = [] as string[]
+    files.forEach((file: string) => {
+        const ext = extname(file)
+        if (!extensions.includes(ext)) extensions.push(ext)
+    })
+    return extensions
+}
+
+/**
+ * iterate extensions and infer their build operations
+ *
+ * @param {string[]} extensions source file extensions
+ * @returns {BUILD_OP_MAP[]} per extension build operations
+ */
+function _inferOpsMapArray(extensions: string[]): BUILD_OP_MAP[] {
+    const opMapArr: BUILD_OP_MAP[] = []
+    extensions.forEach((ext: string) => {
+        const opMap = _inferBuildOpMap(ext)
+        opMapArr.push(opMap)
+    })
+    return opMapArr
+}
+
+/**
+ * infer file extension build operations
+ *
+ * @param {string[]} files source input files
+ * @returns {BUILD_OP_MAP[]} per extension build operations
+ */
+function _inferOps(files: string[]): BUILD_OP_MAP[] {
+    const extensions = _listExtensions(files)
+    const opMapArr: BUILD_OP_MAP[] = _inferOpsMapArray(extensions)
+    return opMapArr
 }
 
 const buildOp = {
-    inferOperation: (dir: string, file: string) => {
-        _inferOperation(dir, file)
+    inferOps: (files: string[]): BUILD_OP_MAP[] => {
+        return _inferOps(files)
     }
 }
 
