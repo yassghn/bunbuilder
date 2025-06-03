5495a1cd1b6df77b1c8eeb8a61ed83defa4b8623
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-03 14:53:23 -0500

    infer source file build operation

--- src/api/buildOp.ts
+++ src/api/buildOp.ts
@@ -1,9 +1,19 @@
 /**
  * buildOp.ts
  */
+import { sep, extname } from 'node:path'
+
+function _inferOperation(dir: string, file: string) {
+    const relativePath = dir + sep + file
+    // ext == '' for extensionless files
+    const ext = extname(relativePath)
+    console.log(ext)
+}
 
 const buildOp = {
-
+    inferOperation: (dir: string, file: string) => {
+        _inferOperation(dir, file)
+    }
 }
 
 export default buildOp
