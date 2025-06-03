636751fc0f71848d14ad3763fd80780d2595a5df
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-03 18:38:53 -0500

    build task

---
+++ src/api/buildTask.ts
@@ -0,0 +1,13 @@
+/**
+ * buildTask.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/buildTask
+ * @property {bunbuilder.module:bunbuilder/api/buildTask} buildTask build tasks
+ */
+
+const buildTask = {
+
+}
+
+export default buildTask
