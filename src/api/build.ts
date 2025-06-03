4cd20b8f91969f71da33f721130abafb42aef774
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-02 20:35:35 -0500

    build all progress

--- src/api/build.ts
+++ src/api/build.ts
@@ -7,17 +7,52 @@
  */
 
 import type { BUNBUILDER_CONFIG } from './types'
+import { readdirSync, lstatSync } from 'node:fs'
+import { sep } from 'node:path'
 
+/**
+ * recursively traverse input directory and aggregate files
+ *
+ * @param {string} dir input directory
+ * @returns {string[]} recursive file aggregate
+ */
+function _getFiles(dir: string): string[] {
+    const retVal = { files: [] as string[] }
+    const files = readdirSync(dir, { encoding: 'utf-8', recursive: true })
+    // filter out directories and add results to return val
+    files.filter((item: string) => {
+        const relativePath = dir + sep + item
+        return lstatSync(relativePath).isFile()
+    }).forEach((file: string) => retVal.files.push(file))
+    return retVal.files
+}
+
+/**
+ * build whole bun app
+ *
+ * @param {BUNBUILDER_CONFIG} config bunbuilder configuration
+ */
 function _buildAll(config: BUNBUILDER_CONFIG) {
-    console.dir(config)
+    // iterate all inputs
+    for (const input of config.options.input) {
+        // lstat input
+        const stat = lstatSync(input)
+        // check for directory
+        if (stat.isDirectory()) {
+            // recursively get all files
+            const files = _getFiles(input)
+            for (const file of files) {
+                console.log(file)
+            }
+        } else {
+        }
+    }
 }
 
 const build = {
-
     all: (config: BUNBUILDER_CONFIG) => {
         _buildAll(config)
     }
-
 }
 
 export default build
