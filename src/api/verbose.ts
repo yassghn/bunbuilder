db25d70837407ce9e998c887e5c5cc782cf733fc
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-03 19:49:09 -0500

    verbose module

---
+++ src/api/verbose.ts
@@ -0,0 +1,13 @@
+/**
+ * verbose.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/verbose
+ * @property {bunbuilder.module:bunbuilder/api/verbose} verbose verbose build output
+ */
+
+const verbose = {
+
+}
+
+export default verbose
