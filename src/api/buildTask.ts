7100dc1652c7adefe9d5b2bf00637ffe8b03c437
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-03 19:12:55 -0500

    build tasks

--- src/api/buildTask.ts
+++ src/api/buildTask.ts
@@ -6,8 +6,37 @@
  * @property {bunbuilder.module:bunbuilder/api/buildTask} buildTask build tasks
  */
 
-const buildTask = {
+import { cp, existsSync, mkdirSync } from 'node:fs'
+import { sep } from 'node:path'
 
+/**
+ * generic copy file
+ *
+ * @param {string} file file to copy
+ * @param {string} dest destinatin directory
+ */
+function _copyFile(dir: string, file: string, dest: string) {
+    const out = dest + sep + file
+    const src = dir + sep + file
+    const options = { recursive: true }
+    cp(src, out, options, (err: any) => {
+        if (err) throw err
+    })
+}
+
+function _makeDestDir(dest: string) {
+    if (!existsSync(dest)) {
+        mkdirSync(dest)
+    }
+}
+
+const buildTask = {
+    copyFile: (dir: string, file: string, dest: string) => {
+        _copyFile(dir, file, dest)
+    },
+    makeDestDir: (dest: string) => {
+        _makeDestDir(dest)
+    }
 }
 
 export default buildTask
