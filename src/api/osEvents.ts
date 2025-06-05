84a174d7cffc9bbf9b29567552342d9d06a9ff9d
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-04 21:58:36 -0500

    add osevents module

---
+++ src/api/osEvents.ts
@@ -0,0 +1,13 @@
+/**
+ * osEvents.ts
+ *
+ * @memberof bunbuilder/api
+ * @module bunbuilder/api/osEvents
+ * @property {bunbuilder.module:bunbuilder/api/osEvents} osEvents handle operating system events
+ */
+
+const osEvents = {
+
+}
+
+export default osEvents
