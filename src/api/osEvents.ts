e20417ff820afeabad8f9ddc7935bbc29376e456
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-04 22:22:11 -0500

    handle os event from ctrl-c on cli process

--- src/api/osEvents.ts
+++ src/api/osEvents.ts
@@ -6,8 +6,27 @@
  * @property {bunbuilder.module:bunbuilder/api/osEvents} osEvents handle operating system events
  */
 
-const osEvents = {
+import shutdown from './shutdown'
+import process from 'node:process'
 
+function _closer(code: NodeJS.Signals) {
+    console.log(code)
+    shutdown.close()
+}
+
+function _addEventListeners() {
+    process.on('SIGINT', _closer)
+    //process.on('beforeExit', _otherClosingWork)
+}
+
+function _handle() {
+    _addEventListeners()
+}
+
+const osEvents = {
+    handle: () => {
+        _handle()
+    }
 }
 
 export default osEvents
