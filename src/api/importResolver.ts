8b196548f1126e1b2a2550ae36f98fef0dc8cf2d
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-12 20:06:09 -0500

    hew paths object from tsconfig compiler options

--- src/api/importResolver.ts
+++ src/api/importResolver.ts
@@ -3,8 +3,74 @@
  */
 
 import data from '../../data/data.json' assert { type: 'json' }
+import { readFileSync } from 'node:fs'
 import { sep } from 'node:path'
 import { cwd } from 'node:process'
+
+/**
+ * @typedef {object} TSCONFIG_COMP_OPS
+ * @type {TSCONFIG_COMP_OPS} tsconfig.json compilerOptions
+ * @property {object} paths tsconfig.json compilerOptions paths
+ */
+interface TSCONFIG_COMP_OPS {
+    paths: object
+}
+
+/**
+ * @typedef {object} TSCONFIG
+ * @type {TSCONFIG} tsconfig.json js object
+ * @property {TSCONFIG_COMP_OPS} compilerOptions tsconfig.json compilerOptions
+ */
+interface TSCONFIG {
+    compilerOptions: TSCONFIG_COMP_OPS
+}
+
+/**
+ * hew tsconfig.json path
+ *
+ * @returns {string} path to tsconfig.json
+ */
+function _hewTsConfigPath(): string {
+    const name = data.options.tsconfigName
+    const path = '.' + sep + name
+    return path
+}
+
+/**
+ * filter comments from tsconfig.json
+ *
+ * @param {string} data unfiltered tsconfig.json
+ * @returns {string} tsconfig.json with all comments filtered out
+ */
+function _filterComments(data: string): string {
+    const regex = /\\"|"(?:\\"|[^"])*"|(\/\/.*)/g
+    const filteredData = data.replace(regex, (m, g) => g ? '' : m)
+    return filteredData
+}
+
+/**
+ * hew tsconfig.json as js object
+ *
+ * @returns {TSCONFIG} tsconfig.json js object
+ */
+function _hewTsConfig(): TSCONFIG {
+    const path = _hewTsConfigPath()
+    const data = readFileSync(path, 'utf-8')
+    const filteredData = _filterComments(data)
+    const tsConfig = JSON.parse(filteredData)
+    return tsConfig
+}
+
+/**
+ * hew paths from tsconfig.json
+ *
+ * @returns {object} object containing tsconfig.json compierOptions.paths
+ */
+function _hewTsConfigPaths(): object {
+    const tsconfig = _hewTsConfig() as TSCONFIG
+    const paths = tsconfig.compilerOptions.paths
+    return paths
+}
 
 /**
  * hew array of files from bun build output artifacts
@@ -82,6 +148,8 @@ function _correctImports(buildOutput: Bun.BuildOutput) {
     const files = _hewBuildArtifactFiles(buildOutput)
     const transpiler = new Bun.Transpiler()
     const prefix = data.options.noBundleHackImportPrefix
+    const paths = _hewTsConfigPaths()
+    console.dir(paths)
     files.forEach(async (file: string) => {
         await _digestImports(transpiler, file, prefix)
     })
