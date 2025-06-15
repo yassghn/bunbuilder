dfed54bfba874c9e441f8901802a57b84155fef2
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-14 19:43:15 -0500

    encapsulate logic for ts config

---
+++ src/api/tsconfig.ts
@@ -0,0 +1,78 @@
+/**
+ * tsconfig.js
+ */
+
+import data from '../../data/data.json' assert { type: 'json' }
+import { readFileSync } from 'node:fs'
+import { sep } from 'node:path'
+
+/**
+ * @typedef {object} TSCONFIG_COMP_OPS
+ * @type {TSCONFIG_COMP_OPS} tsconfig.json compilerOptions
+ * @property {object} paths tsconfig.json compilerOptions paths
+ */
+interface TSCONFIG_COMP_OPS {
+    paths: object
+}
+
+/**
+ * @typedef {object} TSCONFIG
+ * @type {TSCONFIG} tsconfig.json js object
+ * @property {TSCONFIG_COMP_OPS} compilerOptions tsconfig.json compilerOptions
+ */
+interface TSCONFIG {
+    compilerOptions: TSCONFIG_COMP_OPS
+}
+
+/**
+ * hew tsconfig.json path
+ *
+ * @returns {string} path to tsconfig.json
+ */
+function _hewTsConfigPath(): string {
+    const name = data.options.tsconfigName
+    const path = '.' + sep + name
+    return path
+}
+
+/**
+ * filter comments from tsconfig.json
+ *
+ * @param {string} data unfiltered tsconfig.json
+ * @returns {string} tsconfig.json with all comments filtered out
+ */
+function _filterComments(data: string): string {
+    const regex = /\\"|"(?:\\"|[^"])*"|(\/\/.*)/g
+    const filteredData = data.replace(regex, (m, g) => (g ? '' : m))
+    return filteredData
+}
+
+/**
+ * hew tsconfig.json as js object
+ *
+ * @returns {TSCONFIG} tsconfig.json js object
+ */
+function _hewTsConfig(): TSCONFIG {
+    const path = _hewTsConfigPath()
+    const data = readFileSync(path, 'utf-8')
+    const filteredData = _filterComments(data)
+    const tsConfig = JSON.parse(filteredData)
+    return tsConfig
+}
+
+/**
+ * hew paths from tsconfig.json
+ *
+ * @returns {object} object containing tsconfig.json compierOptions.paths
+ */
+function _hewTsConfigPaths(): object {
+    const tsconfig = _hewTsConfig() as TSCONFIG
+    const paths = tsconfig.compilerOptions.paths
+    return paths
+}
+
+function hewTsConfigPaths(): object {
+    return _hewTsConfigPaths()
+}
+
+export { hewTsConfigPaths }
